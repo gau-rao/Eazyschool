61b9f7336276e16f0287322b3ce13f09ea6952b7
Gaurao <gauraonandekar99@gmail.com>
2024-09-03 18:20:28 +0530

    password hashing using bcryptencoder

--- src/main/java/com/easybytes/eazyschool/EazySchoolApplication.java
+++ src/main/java/com/easybytes/eazyschool/EazySchoolApplication.java
@@ -5,9 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 @EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 @EnableJpaRepositories("com.easybytes.eazyschool.repository")
 @EntityScan("com.easybytes.eazyschool.model")
+@EnableWebSecurity
 @SpringBootApplication
 public class EazySchoolApplication {
 
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -28,3 +28,7 @@ VALUES ('ADMIN',CURDATE(),'DBA');
 
 INSERT INTO `roles` (`role_name`,`created_at`, `created_by`)
 VALUES ('STUDENT',CURDATE(),'DBA');
+
+
+INSERT INTO `person` (`name`,`email`,`mobile_number`,`pwd`,`role_id`,`created_at`, `created_by`)
+VALUES ('Admin','admin@eazyschool.com','3443434343','ajtgjm', 1 ,CURDATE(),'DBA');
