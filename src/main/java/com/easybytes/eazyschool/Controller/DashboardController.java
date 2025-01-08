e87b99986b3a3f296d122f6aa9be3b47b726cda5
gau-rao <gauraonandekar99@gmail.com>
2025-01-08 22:18:00 +0530

    debug logging

--- src/main/java/com/easybytes/eazyschool/Controller/DashboardController.java
+++ src/main/java/com/easybytes/eazyschool/Controller/DashboardController.java
@@ -28,4 +28,13 @@ public class DashboardController {
         return "dashboard.html";
     }
 
+    private void logMessages() {
+        log.error("Error message from the Dashboard page");
+        log.warn("Warning message from the Dashboard page");
+        log.info("Info message from the Dashboard page");
+        log.debug("Debug message from the Dashboard page");
+        log.trace("Trace message from the Dashboard page");
+    }
+
+
 }
